2b2be09cc559f042d9397c45bc37960037cdf3db
Unguturu Loka Tarini <lokatarini.unguturu@gmail.com>
2026-07-08 07:25:49 +0530

    Format project using Prettier

--- script.js
+++ script.js
@@ -2,93 +2,88 @@ let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 displayTasks();
 
-function addTask(){
+function addTask() {
+  let title = document.getElementById("title").value.trim();
+  let description = document.getElementById("description").value.trim();
+  let dueDate = document.getElementById("dueDate").value;
+  let status = document.getElementById("status").value;
 
-    let title=document.getElementById("title").value.trim();
-    let description=document.getElementById("description").value.trim();
-    let dueDate=document.getElementById("dueDate").value;
-    let status=document.getElementById("status").value;
+  if (title === "") {
+    alert("Please enter task title");
+    return;
+  }
 
-    if(title===""){
-        alert("Please enter task title");
-        return;
-    }
+  let task = {
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    status: status,
+  };
 
-    let task={
-        title:title,
-        description:description,
-        dueDate:dueDate,
-        status:status
-    };
+  tasks.push(task);
 
-    tasks.push(task);
+  saveTasks();
 
-    saveTasks();
+  displayTasks();
 
-    displayTasks();
-
-    document.getElementById("title").value="";
-    document.getElementById("description").value="";
-    document.getElementById("dueDate").value="";
-    document.getElementById("status").value="Todo";
-
+  document.getElementById("title").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("dueDate").value = "";
+  document.getElementById("status").value = "Todo";
 }
 
-function displayTasks(){
+function displayTasks() {
+  let taskList = document.getElementById("taskList");
 
-    let taskList=document.getElementById("taskList");
+  taskList.innerHTML = "";
 
-    taskList.innerHTML="";
+  tasks.forEach(function (task, index) {
+    let div = document.createElement("div");
 
-    tasks.forEach(function(task,index){
+    div.className = "task";
 
-        let div=document.createElement("div");
+    div.innerHTML =
+      "<h3>" +
+      task.title +
+      "</h3>" +
+      "<p><b>Description:</b> " +
+      task.description +
+      "</p>" +
+      "<p><b>Due Date:</b> " +
+      task.dueDate +
+      "</p>" +
+      "<p><b>Status:</b> " +
+      task.status +
+      "</p>" +
+      "<button onclick='updateTask(" +
+      index +
+      ")'>Update Status</button>" +
+      "<button onclick='deleteTask(" +
+      index +
+      ")'>Delete</button>";
 
-        div.className="task";
-
-        div.innerHTML=
-        "<h3>"+task.title+"</h3>"+
-        "<p><b>Description:</b> "+task.description+"</p>"+
-        "<p><b>Due Date:</b> "+task.dueDate+"</p>"+
-        "<p><b>Status:</b> "+task.status+"</p>"+
-        "<button onclick='updateTask("+index+")'>Update Status</button>"+
-        "<button onclick='deleteTask("+index+")'>Delete</button>";
-
-        taskList.appendChild(div);
-
-    });
-
+    taskList.appendChild(div);
+  });
 }
 
-function updateTask(index){
+function updateTask(index) {
+  if (tasks[index].status === "Todo") tasks[index].status = "In Progress";
+  else if (tasks[index].status === "In Progress") tasks[index].status = "Done";
+  else tasks[index].status = "Todo";
 
-    if(tasks[index].status==="Todo")
-        tasks[index].status="In Progress";
+  saveTasks();
 
-    else if(tasks[index].status==="In Progress")
-        tasks[index].status="Done";
-
-    else
-        tasks[index].status="Todo";
-
-    saveTasks();
-
-    displayTasks();
-
+  displayTasks();
 }
 
-function deleteTask(index){
+function deleteTask(index) {
+  tasks.splice(index, 1);
 
-    tasks.splice(index,1);
+  saveTasks();
 
-    saveTasks();
-
-    displayTasks();
-
+  displayTasks();
 }
 
-function saveTasks(){
-
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
